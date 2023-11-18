c0323f8445b62441bb3a67733d6d9e8289b39c42
Alexander Royal <alexander.de.royal@gmail.com>
2023-11-18 18:52:14 +0545

    completed about us section

--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
-
+import {SubHeading, MenuItem} from '../../components';
+import {images, data} from '../../constants';
 import './SpecialMenu.css';
 
 const SpecialMenu = () => (
-  <div>
-    SpecialMenu
+  <div className="app__specialMenu flex__center section__padding" id="menu">
+    <div className="app_specialMenu-title">
+        <SubHeading title="Menu that fits your  Palatte"/>
+        <h1 className="headtext__cormorant">Today's Special</h1>
+    </div>
+      <div className="app__specialMenu-menu">
+          <div className="app_specialMenu-menu_wine flex__center">
+              <p className="app__specialmenu_menu_heading">Wine & Beer</p>
+              <div className="app__specialMenu_menu_items">
+                  {data.wines.map((wine, index) => {
+                      <p>{wine.title}</p>
+                  })}
+              </div>
+          </div>
+      </div>
   </div>
 );
 
